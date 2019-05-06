888fa066b4a9dd472b4176912baef0b6f7ce6ad5
xchen218 <xchen218@binghamton.edu>
2019-05-05 23:20:50 -0400

    1

---
+++ app/src/main/java/com/xchen218/cs541p8/MainActivity.java
@@ -0,0 +1,13 @@
+package com.xchen218.cs541p8;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
